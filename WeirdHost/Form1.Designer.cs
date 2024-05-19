54691507f60e46bb11af0f3a67fbd3a56717d30f
jungha0110 <sjh0521a@gmail.com>
2024-05-19 15:52:18 +0900

    Add project files.

---
+++ WeirdHost/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace WeirdHost
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1280, 720);
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                                    | System.Windows.Forms.AnchorStyles.Left)
+                                    | System.Windows.Forms.AnchorStyles.Right;
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.webView21);
+            this.Name = "WeirdHost";
+            this.Text = "WeirdHost";
+            this.Icon = Properties.Resources.weirdhost;
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ WeirdHost/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace WeirdHost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.Navigate("https://hub.weirdhost.xyz/");
+        }
+    }
+}
+
